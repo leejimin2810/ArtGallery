1240c984ba998858b4701a45585c4cbe01fd8a92
Jimin <leminh28101999@gmail.com>
2024-07-12 23:13:07 +0700

    Add UI Layout

---
+++ ArtGallery/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Error404()
+        {
+            return View();
+        }
+        public IActionResult LoginManagement()
+        {
+            return View();
+        }
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+        public IActionResult Artwork()
+        {
+            return View();
+        }
+        public IActionResult Auction()
+        {
+            return View();
+        }
+        public IActionResult Exhibition()
+        {
+            return View();
+        }
+        public IActionResult Payment()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Blank()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
--- ArtGallery/Controllers/HomeController.cs
+++ ArtGallery/Controllers/HomeController.cs
@@ -6,27 +6,22 @@ namespace ArtGallery.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
 
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
     }
 }
