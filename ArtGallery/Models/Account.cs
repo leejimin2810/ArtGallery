c4755c56a388f2f16da76ff6dfe2cc9b4f7177d0
Duc Lam <dinhduclam2001@gmail.com>
2024-07-21 04:16:21 +0700

    edit account model

--- ArtGallery/Models/Account.cs
+++ ArtGallery/Models/Account.cs
@@ -10,5 +10,7 @@ namespace ArtGallery.Models
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
     }
 }
