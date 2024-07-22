605ce64df9f242b9f8620ebfa76a0d75a20fc5db
Jimin <leminh28101999@gmail.com>
2024-07-22 15:09:24 +0700

    Add Category Validation

---
+++ ArtGallery/Validations/CategoryValidation.cs
@@ -0,0 +1,20 @@
+﻿using ArtGallery.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtGallery.Validations
+{
+    public class CategoryValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var artwork = (Artwork)validationContext.ObjectInstance;
+
+            if (artwork.Category == Category.Auction && artwork.Price.HasValue)
+            {
+                return new ValidationResult("Auction items should not have a default price.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
