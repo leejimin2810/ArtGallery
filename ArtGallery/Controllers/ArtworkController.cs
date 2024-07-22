bd48044d043e6ae8131a66f887690c10aa49763f
Jimin <leminh28101999@gmail.com>
2024-07-22 15:12:37 +0700

    CRUD for Artwork

--- ArtGallery/Controllers/ArtworkController.cs
+++ ArtGallery/Controllers/ArtworkController.cs
@@ -13,39 +13,144 @@ namespace ArtGallery.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
+
         public ArtworkController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
+
         public async Task<IActionResult> Index()
         {
             var artworks = await _context.Artworks.ToListAsync();
             var artworkViews = _mapper.Map<List<ArtworkView>>(artworks);
             return View(artworkViews);
         }
-        public async Task<IActionResult> Detail()
+        public async Task<IActionResult> Index1()
         {
-            //var artworks = await _context.Artworks.ToListAsync();
-            //var artworkViews = _mapper.Map<List<ArtworkView>>(artworks);
-            return View();
+            var artworks = await _context.Artworks.ToListAsync();
+            var artworkViews = _mapper.Map<List<ArtworkView>>(artworks);
+            return View(artworkViews);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var artwork = await _context.Artworks.FindAsync(id);
+            if (artwork == null)
+            {
+                return NotFound();
+            }
+            var artworkView = _mapper.Map<ArtworkView>(artwork);
+            return View(artworkView);
         }
 
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Artwork artWork)
+        public async Task<IActionResult> Create(ArtworkView artworkView)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                _context.Add(artWork);
+                var artwork = _mapper.Map<Artwork>(artworkView);
+                _context.Add(artwork);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("index");
+                return RedirectToAction("Index1");
             }
-            return View();
+            return View(artworkView);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var artwork = await _context.Artworks.FindAsync(id);
+            if (artwork == null)
+            {
+                return NotFound();
+            }
+            var artworkView = _mapper.Map<ArtworkView>(artwork);
+            return View(artworkView);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ArtworkView artworkView)
+        {
+            if (id != artworkView.ArtworkId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var artwork = _mapper.Map<Artwork>(artworkView);
+                _context.Update(artwork);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index1");
+            }
+            return View(artworkView);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var artwork = await _context.Artworks.FindAsync(id);
+            if (artwork == null)
+            {
+                return NotFound();
+            }
+            var artworkView = _mapper.Map<ArtworkView>(artwork);
+            return View(artworkView);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artwork = await _context.Artworks.FindAsync(id);
+            _context.Artworks.Remove(artwork);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index1");
+        }
+
+        private bool ArtworkExists(int id)
+        {
+            return _context.Artworks.Any(e => e.ArtworkId == id);
+        }
+        //private readonly ApplicationDbContext _context;
+        //private readonly IMapper _mapper;
+        //public ArtworkController(ApplicationDbContext context, IMapper mapper)
+        //{
+        //    _context = context;
+        //    _mapper = mapper;
+        //}
+        //public async Task<IActionResult> Index()
+        //{
+        //    var artworks = await _context.Artworks.ToListAsync();
+        //    var artworkViews = _mapper.Map<List<ArtworkView>>(artworks);
+        //    return View(artworkViews);
+        //}
+        //public async Task<IActionResult> Detail()
+        //{          
+        //    return View();
+        //}
+
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(Artwork artWork)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        _context.Add(artWork);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction("index");
+        //    }
+        //    return View();
+        //}
     }
 }
